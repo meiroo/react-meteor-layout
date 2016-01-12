dbe9868ee1cec4c41d695284c3b9ab48e9fb868a
merioo <meiroo@outlook.com>
2016-01-12 17:54:58 +0800

    grid

--- app/components/MLayout.jsx
+++ app/components/MLayout.jsx
@@ -12,16 +12,17 @@ import FloatingActionButton from 'material-ui/lib/floating-action-button';
 export default class MLayout extends Component {
 
     render() {
-        return (
-            <ReactGridLayout className="layout" isDraggable={true}
-            isResizable={true} autoSize={false}>
-              <div key={1}>1</div>
-              <div key={2}>2</div>
-              <div key={3}>3</div>
-              <div key={4}>
-              <RaisedButton label="Secondary" secondary={true} />
-              </div>
-            </ReactGridLayout>
-        );
+      var layout_grid = [{x: 1, y: 1, w: 2, h: 2, i:1},{x: 1, y: 1, w: 3, h: 2, i:2},{x: 1, y: 1, w: 2, h: 3, i:3},{x: 1, y: 1, w: 3, h: 3, i:4}];
+      return (
+          <ReactGridLayout className="layout" layout={layout_grid} isDraggable={true}
+          isResizable={true} autoSize={true} cols={30} rowHeight={60}>
+            <div key={1}>353453451</div>
+            <div key={2}>2sdfsdfsd</div>
+            <div key={3}>3</div>
+            <div key={4}>
+            <RaisedButton label="Secondary" secondary={true} />
+            </div>
+          </ReactGridLayout>
+      );
     }
 }
